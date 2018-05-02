01e61c1a73d0da7595b386f2de217f92dbe98086
KrYounger <kryounger@gmail.com>
2018-05-02 13:59:03 -0400

    fixes

--- web2/web2.go
+++ web2/web2.go
@@ -1,28 +1,31 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-helloHandler := func(w http.ResponseWriter, r *http.Request) {
-w.Header().Set("Content-Type", “text/plain”) // optional
-w.WriteHeader(http.StatusOK) // optional
-fmt.Fprintln(w, "Hello, 世界”)
-        }
+	helloHandler := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain") // optional
+		w.WriteHeader(http.StatusOK)                 // optional
+		fmt.Fprintln(w, "Hello, 世界")
+	}
 
-helloHandler = log(helloHandler) // wraps the orig handler
+	helloHandler = logg(helloHandler) // wraps the orig handler
 
-   http.HandleFunc(“/", helloHandler)
-   http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", helloHandler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
-func log(h http.Handler) http.Handler {
-  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request)
-  {
-    log.Println("Before")
-    h.ServeHTTP(w, r) // call original handler
-    log.Println("After")
-  })
+func logg(h http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Before")
+		h.ServeHTTP(w, r) // call original handler
+		log.Println("After")
+	})
 }
